80a4467bebc646868956b4b4e07e50ab8263c71d
toprak255 <toprakaydiner6@gmail.com>
2023-02-05 13:30:09 +0300

    added player collision

--- tile-renderer.c
+++ tile-renderer.c
@@ -1,14 +1,13 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <GL/glut.h>
+#include <stdbool.h>
 // #include "stb_image.h"
 typedef struct
 {
  int w,a,d,s;                     //button state on off
 }ButtonKeys; ButtonKeys Keys;
-float playerX, playerY;
-float frame1,frame2,fps;
+int playerX, playerY;
 int coordy,coordx;
 void drawPlayer()
 {
@@ -22,6 +21,7 @@ int mapX=32,mapY=16,mapCubeSize=32;
 int map[512]={0,1,1,1,1,1,1,1,1,1,1,1,1};
 void renderWorld()
 {
+ map[76] = 1;
  int x,y,xo,yo;
  for(y=0;y<mapY;y++)
  {
@@ -30,14 +30,14 @@ void renderWorld()
    if(map[y*mapX+x]==1){ glColor3f(1,1,1);} else{ glColor3f(0,0,0);}
    xo=x*mapCubeSize; yo=y*mapCubeSize;
    glBegin(GL_QUADS); 
-     glVertex2i( 0          +xo+1, 0          +yo+1); 
-     glVertex2i( 0          +xo+1, mapCubeSize+yo-1); 
-     glVertex2i( mapCubeSize+xo-1, mapCubeSize+yo-1);  
-     glVertex2i( mapCubeSize+xo-1, 0          +yo+1); 
-          //   glVertex2i(xo,yo);
-          //   glVertex2i(xo,yo+mapCubeSize);
-          //   glVertex2i(xo+mapCubeSize,yo+mapCubeSize);
-          //   glVertex2i(xo+mapCubeSize,yo);
+    //  glVertex2i( 0          +xo+1, 0          +yo+1); 
+    //  glVertex2i( 0          +xo+1, mapCubeSize+yo-1); 
+    //  glVertex2i( mapCubeSize+xo-1, mapCubeSize+yo-1);  
+    //  glVertex2i( mapCubeSize+xo-1, 0          +yo+1); 
+            glVertex2i(xo,yo);
+            glVertex2i(xo,yo+mapCubeSize);
+            glVertex2i(xo+mapCubeSize,yo+mapCubeSize);
+            glVertex2i(xo+mapCubeSize,yo);
    glEnd();
   } 
  } 
@@ -48,7 +48,7 @@ void keyDown(char key, int x, int y)
     if(key=='a'){ Keys.a=1;} 
     if(key=='w'){ Keys.w=1;}
     if(key=='s'){ Keys.s=1;}
-    printf("%.0f %.0f\n", playerX, playerY);
+    printf("%i %i\n", playerX, playerY);
     glutPostRedisplay();
     if (playerX > 1020){playerX = 0;}
     if (playerX < 0){playerX = 1020;}
@@ -67,23 +67,73 @@ void init()
 {
     glClearColor(0.3, 0.1, 0.1, 0);
     gluOrtho2D(0, 1024, 512, 0);
-    playerX = 400, playerY = 200; // setting player position
+    playerX = 399, playerY = 199; // setting player position
 }
+int cornerCoords[8];
+int corner()
+{
+    cornerCoords[0]=playerX-4;cornerCoords[1]=playerY-4;//TL
+    cornerCoords[2]=playerX+4;cornerCoords[3]=playerY-4;//TR
+    cornerCoords[4]=playerX-4;cornerCoords[5]=playerY+4;//BL
+    cornerCoords[6]=playerX+4;cornerCoords[7]=playerY+4;//BR
+}
+int dotCoord(int coordX, int coordY){
+    // printf("%d,%d  %d,%d\n%d,%d  %d,%d\n#========\n",
+    // cornerCoords[0],cornerCoords[1],
+    // cornerCoords[2],cornerCoords[3],
+    // cornerCoords[4],cornerCoords[5],
+    // cornerCoords[6],cornerCoords[7] 
+    // );
+    int y=coordY/mapCubeSize;
+    int x=coordX/mapCubeSize;
+    int cornerpos = y*mapX+x;
+    return(cornerpos);
+}
+bool collision(int direction){
+    corner();
+    /* 
+    direction:
+        0=left
+        1=right
+        2=top
+        3=bottom
+    */
+   if(direction==0)
+   {
+    if(map[dotCoord(cornerCoords[0]-2,cornerCoords[1]+1)]== 0 && (map[dotCoord(cornerCoords[4]-2,cornerCoords[5]-1)]==0)){return true;}
+    else{return false;}
+   }
+   if(direction==1)
+   {
+    if(map[dotCoord(cornerCoords[2]+1,cornerCoords[3]+1)]== 0 && (map[dotCoord(cornerCoords[6]+1,cornerCoords[7]-1)]==0)){return true;}
+    else{return false;}
+   }
+   if(direction==2)
+   {
+    if(map[dotCoord(cornerCoords[0]+1,cornerCoords[1]-1)]== 0 && (map[dotCoord(cornerCoords[2]-1,cornerCoords[3]-1)]==0)){return true;}
+    else{return false;}
+   }
+   if(direction==3)
+   {
+    if(map[dotCoord(cornerCoords[4]+1,cornerCoords[5]+1)]== 0 && (map[dotCoord(cornerCoords[6]-1,cornerCoords[7]+1)]==0)){return true;}
+    else{return false;}
+   }
+   
 
+
+
+}
 void display()
-{   coordx=(playerX/mapCubeSize);
-    coordy=(playerY/mapCubeSize);
-   int coord = coordy*mapX+coordx;
-    if (map[coord]==1){
-
-    }
-    else{
-        if(Keys.a==1){ playerX-=4;} 	
-        if(Keys.d==1){ playerX+=4; } 
-        if(Keys.s==1){ playerY+=4; } 	
-        if(Keys.w==1){ playerY-=4; } 
-        }
-    printf("%i %i\n", coordx, coordy);
+{
+        if(Keys.a==1 && collision(0))
+        { playerX-=4;} 	
+        if(Keys.d==1 && collision(1))
+        { playerX+=4; } 
+        if(Keys.s==1 && collision(3))
+        { playerY+=4; } 	
+        if(Keys.w==1 && collision(2))
+        { playerY-=4; } 
+    // printf("%i %i\n", coordx, coordy);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     renderWorld();
     drawPlayer();
@@ -92,11 +142,12 @@ void display()
 
 int main(int argc, char *argv[])
 {
+    
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
     glutInitWindowSize(1024, 512);
     glutInitWindowPosition(1920/4,1080/4);
-    glutCreateWindow("allah");
+    glutCreateWindow("test");
     init();
     glutDisplayFunc(display);
     glutKeyboardFunc(keyDown);
